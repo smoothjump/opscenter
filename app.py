ceecc95c71c89ac63638db406f17835492558c18
smoothjump <smoothjump@gmail.com>
2019-06-20 18:56:50 +0800

    ===

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from utils.log import logger
 from aiohttp import web
 import asyncio
+from utils.db import create_pool
+from models.model import Users
 
 
 async def index(request):
+    user = Users(name="BBB", email="1@1.cn")
+    await user.save()
     return web.Response(body='Awesome')
+
+
+async def ui(request):
+    return web.Response()
 
 
 async def init():
@@ -14,6 +22,7 @@ async def init():
     await runner.setup()
     site = web.TCPSite(runner, 'localhost', 9000)
     await site.start()
+    await create_pool(user='root', password='123', db='ops')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
