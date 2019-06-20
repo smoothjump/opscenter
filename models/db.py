5f89a7bc6e91608516aac48e43f96874f96c1d98
smoothjump <smoothjump@gmail.com>
2019-06-20 16:08:00 +0800

    ignore file added

--- models/db.py
+++ models/db.py
@@ -39,13 +39,16 @@ async def select(sql, args=None, size=None):
         return rs
 
 
-async def execute(sql, args):
+async def execute(sql, args=None):
     logger.info(sql)
     logger.info('Parameter: %s' % str(args))
     async with __pool.acquire() as conn:
         try:
             cur = await conn.cursor()
-            await cur.execute(sql.replace('?', '%s'), args)
+            if args:
+                await cur.execute(sql.replace('?', '%s'), args)
+            else:
+                await cur.execute(sql)
             affected = cur.rowcount
             await cur.close()
             logger.debug('Affected rows: %s' % str(affected))
@@ -54,11 +57,16 @@ async def execute(sql, args):
         return affected
 
 
-async def sj():
-    r = await asyncio.sleep(10)
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(create_pool(loop, user='root', password='123', db='ops'))
     loop.run_until_complete(select("select * from user where reserved=?", args=(12.34)))
-    loop.run_until_complete(execute("insert into test values(?, ?)", args=[2, "RRT"]))
+    # loop.run_until_complete(execute("insert into test values(?, ?)", args=[2, "RRT"]))
+    loop.run_until_complete(execute("show tables"))
